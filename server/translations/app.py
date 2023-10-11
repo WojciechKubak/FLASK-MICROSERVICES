681c83a40af6d1976999c11ea418fb46f81ad2d9
Wojciech_Kubak <wojciech.kubak@smartepc.pl>
2023-10-11 11:07:05 +0200

    ADD: microservice base struct with proxy config

---
+++ server/translations/app.py
@@ -0,0 +1,19 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask import Flask, Response, make_response
+
+app = Flask(__name__)
+
+
+def create_app() -> Flask:
+
+    with app.app_context():
+
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+        )
+
+        @app.route('/')
+        def index() -> Response:
+            return make_response({'message': 'Tanslations home page'}, 200)
+
+        return app
